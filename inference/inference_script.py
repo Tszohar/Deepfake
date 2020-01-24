9035c1e8bbfee2be6e4ab42a6dd1d22604da8e9e
Tsofit Zohar <tsofit.bk@gmail.com>
2020-01-24 11:18:30 +0200

    performance improvements

--- inference/inference_script.py
+++ inference/inference_script.py
@@ -1,47 +1,26 @@
 import glob
 import os
-import shutil
 import sys
-import zipfile
 
+import inf_config
 import pandas as pd
-
-from inference import inf_config
-from inference.video_handler import VideoHandler
-
-
-def extract_zip(zip_file_path: str, dst_folder: str):
-    """
-    :param zip_file_path: file path to a zip file to be extracted to dst_folder
-    :param dst_folder: destination folder for the unzipped files
-    :return: This function receives a zip file and extract it to dst_folder
-    """
-
-    with zipfile.ZipFile(zip_file_path) as zip:
-        if os.path.isdir(dst_folder):
-            shutil.rmtree(dst_folder)
-        os.makedirs(dst_folder)
-        print('Extracting zip')
-        zip.extractall(dst_folder)
-        print('Extraction Completed')
-
+from video_handler import VideoHandler
 
 if __name__ == "__main__":
     print(sys.argv)
     videos_folder = sys.argv[1]
-    # videos_folder = os.path.splitext(zip_file_path)[0]
-    # extract_zip(zip_file_path=zip_file_path, dst_folder=videos_folder)
     videos_file_list = glob.glob1(videos_folder, "*.mp4")
-    video_handler = VideoHandler(image_size=inf_config.image_size, frame_decimation=inf_config.frame_decimation,
-                                 output_path=inf_config.output_path, model_path=inf_config.model)
+    video_handler = VideoHandler(image_size=inf_config.image_size,
+                                 frame_decimation=inf_config.frame_decimation,
+                                 output_path=inf_config.output_path,
+                                 model_path=inf_config.model)
     submission_results = {}
     for video_file_name in videos_file_list:
         video_file_path = os.path.join(videos_folder, video_file_name)
         result = video_handler.handle(video_file_path=video_file_path)
         submission_results.update({(video_file_name, result)})
         print('{} FAKE probability is: {}'.format(video_file_name, result))
-    # submission_path = os.path.join(os.path.dirname(zip_file_path), 'submission.csv')
     dataframe = pd.DataFrame(list(submission_results.items()), columns=['filename', 'label'])
-    dataframe.to_csv('submission.csv')
+    dataframe.to_csv('submission.csv', index=False)
 
 
--- inference/video_pre_processor.py
+++ inference/video_pre_processor.py
@@ -1,10 +1,11 @@
 import os
 import shutil
+from typing import List, Callable
 
 import cv2
 import numpy as np
+import torch
 from PIL import Image
-
 from frame_handler import FrameHandler
 
 
@@ -20,38 +21,51 @@ class VideoPreProcessor:
     def get_dst_folder(cls, video_file_path: str) -> str:
         return os.path.splitext(video_file_path)[0]
 
-    def convert_to_frames(self, video_file_path: str, frame_decimation: int) -> None:
+    def convert_to_frames(self,
+                          video_file_path: str,
+                          frame_decimation: int,
+                          preprocessing_transform: Callable) -> List[torch.Tensor]:
         """
         :param video_file_path: video file path
-        :param root_folder: root directory to save the output frames for video_file_path
         :param frame_decimation: video decimation to create frames
+        :param preprocessing_transform: Transform that preprocesses the image
         :return: None
         """
-        dst_folder = self._working_folder
-        if os.path.isdir(dst_folder):
-            shutil.rmtree(dst_folder)
-        os.makedirs(dst_folder)
-
         vidcap = cv2.VideoCapture(video_file_path)
-
-        count = 0
-        while True:
-            image = None
+        frame_num = int(vidcap.get(cv2.CAP_PROP_FRAME_COUNT))
+        frame_list = []
+        for frame_idx in range(0, frame_num, frame_decimation):
             for i in range(frame_decimation):
-                success, image = vidcap.read()
-                if not success:
-                    return
+                vidcap.grab()
+            success, image = vidcap.retrieve()
+            if not success:
+                break
             assert image is not None
             image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
             image = Image.fromarray(image)
             img_cropped = self._frame_handler.process(image)
             if img_cropped is None:
                 continue
-            np_image = img_cropped.numpy()
+            transformed_image = preprocessing_transform(img_cropped)
+            frame_list.append(transformed_image)
+
+        return frame_list
+
+    @classmethod
+    def dump_to_files(cls, frames: List[torch.Tensor], dst_folder: str) -> None:
+        """
+        Dumps list of frames to files
+        :param frames: list of torch tensor, each corresponds to a frame in a video
+        :param dst_folder: destination folder to save data in
+        """
+        if os.path.isdir(dst_folder):
+            shutil.rmtree(dst_folder)
+        os.makedirs(dst_folder)
+        for idx, frame in enumerate(frames):
+            np_image = frame.numpy()
             trans_image = np.transpose(np_image, [1, 2, 0])
             trans_image = trans_image / np.max(np.abs(trans_image))
             trans_image = trans_image / 2 + 0.5
             trans_image = (trans_image * 255)
             trans_image = cv2.cvtColor(trans_image, cv2.COLOR_RGB2BGR)
-            cv2.imwrite(os.path.join(dst_folder, "frame{:03d}.jpg".format(count)), trans_image)
-            count += 1
+            cv2.imwrite(os.path.join(dst_folder, "frame{:03d}.jpg".format(idx)), trans_image)
